91cd8e4c169c9381fd50b0942bda9a490ae0b5de
ieumm <tngudwkd3@naver.com>
2024-11-27 16:19:23 +0900

    Add files via upload

---
+++ 소트인사이드.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	int n;
+	int arr[10] = { 0, };
+
+	scanf("%d", &n);
+	while (n > 0)
+	{
+		arr[n % 10]++;
+		n /= 10;
+	}
+	for (int i = 9; i >= 0; i--)
+	{
+		for (int j = 0; j < arr[i]; j++)
+			printf("%d", i);
+	}
+	return 0;
+}
